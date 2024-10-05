e7e92551da3d62f5c25c9ed46b95dd718c617091
AmnaAmir6 <amnamair605@gmail.com>
2024-10-06 01:17:05 +0500

    Sidebar component

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/NavBar'
+import SideBar from './components/SildeBar'
 
 function App() {
 
   return (
     <>
     <div className="wrapper">
+      <SideBar />
       <div className="main">
         <Navbar />
         <div className="container-fluid p-0">
